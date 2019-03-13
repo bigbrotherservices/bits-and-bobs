d357ca83c0f180dfc29408220f6279a6d8a030b5
Quinn <quinntdip@gmail.com>
2019-03-12 20:19:08 -0500

    Create fizzbuzz1.js

---
+++ fizzbuzz1.js
@@ -0,0 +1,15 @@
+// Our first solution to Fizz Buzz
+for (var i = 1; <= 100; i++) {
+  if ((i%3 == 0) && (i%5 != 0) ) {
+  console.log('Fizz');
+ }
+ if ((i%3 != 0) && (i%5 == 0) ) {
+  console.log('Buzz');
+ } 
+ if ((i%3 == 0) && (i%5 == 0) ) {
+  console.log('FizzBuzz');
+ }
+ if ((i%3 != 0) && (i%5 != 0) ) {
+  console.log(i);
+ }
+}
